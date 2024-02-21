879deb58c0a25fda265e252e56aa0fc8e716e3a9
redl <155835149+lb-red@users.noreply.github.com>
2024-02-21 18:46:18 +0800

    Prompt user for choice
    Asks the user for a choice, only one of the choice is accepted (case
    insensitive).

---
+++ javascript.js
@@ -0,0 +1,17 @@
+const CHOICES = ["ROCK", "PAPER", "SCISSORS"];
+
+let userChoice = prompt("Rock, paper, or scissors?");
+
+if (userChoice == undefined || userChoice == "") {
+  console.log("Invalid input.");
+} else {
+  userChoice = userChoice.toUpperCase();
+
+  if (userChoice == CHOICES[0]
+    || userChoice == CHOICES[1]
+    || userChoice == CHOICES[2]) {
+    
+  } else {
+    console.log("Not in the choices.");
+  }
+}
