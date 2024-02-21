dfac6440e96708170c0ec90ab60b81d9a8770e64
redl <155835149+lb-red@users.noreply.github.com>
2024-02-21 19:27:14 +0800

    Play a round and print the result.
    Plays a single round and added logic on what the results will be.

--- javascript.js
+++ javascript.js
@@ -8,6 +8,26 @@ function getComputerChoice() {
   return CHOICES[random];
 }
 
+function playRound(playerSelection, computerSelection) {
+  let result = `User chose ${playerSelection} and computer chose ${computerSelection}. `;
+  
+  // same choice
+  if (playerSelection == computerSelection) {
+    result += "It's a draw.";
+    // Rock vs Paper
+  } else if (playerSelection == CHOICES[0] && computerSelection == CHOICES[1]
+    // Paper vs Scissors
+    || playerSelection == CHOICES[1] && computerSelection == CHOICES[2]
+    // Scissors vs Rock
+    || playerSelection == CHOICES[2] && computerSelection == CHOICES[0]) {
+    result += "Computer wins.";
+    // Other case where user wins
+  } else {
+    result += "User wins.";
+  }
+  return result;
+}
+
 // Ensures user input is one of the choices. Case insensitive
 if (userChoice == undefined || userChoice == "") {
   console.log("Invalid input.");
@@ -17,7 +37,9 @@ if (userChoice == undefined || userChoice == "") {
   if (userChoice == CHOICES[0]
     || userChoice == CHOICES[1]
     || userChoice == CHOICES[2]) {
-    
+    result = playRound(userChoice, computerChoice);
+    console.log(result);
+    alert(result);
   } else {
     console.log("Not in the choices.");
   }
